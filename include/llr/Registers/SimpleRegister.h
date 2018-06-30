598bad275d8347806538366d3537e21bd448412e
Sergey Gvozdarev <gvozdarev.sa@gmail.com>
2018-06-30 21:32:50 +0300

    New class SimpleRegister

---
+++ include/llr/Registers/SimpleRegister.h
@@ -0,0 +1,58 @@
+//===- llr/Registers/Register.h - Memory access interface ---*- C++ -*-----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLR_REGISTERS_SIMPLEREGISTER_H
+#define LLR_REGISTERS_SIMPLEREGISTER_H
+
+#include "Register.h"
+
+#include "llvm/ADT/ArrayRef.h"
+
+#include <vector>
+
+namespace llvm {
+  class APInt;
+  class APFloat;
+}
+
+namespace llr {
+
+class RegisterAccessResult;
+
+
+
+class SimpleRegister : public Register {
+  using APFloat = llvm::APFloat;
+  using APInt = llvm::APInt;
+
+public:
+
+  SimpleRegister(const SimpleRegister&) = delete;
+  SimpleRegister(SimpleRegister&&) = default;
+
+  SimpleRegister(unsigned RegId, unsigned ClId, std::size_t size) :
+    Register(RegId, ClId, size) {
+    data_storage.resize(size);
+  }
+
+  virtual RegisterAccessResult get() override;
+
+
+  virtual RegisterAccessResult set(const llvm::ArrayRef<uint8_t> data, SetMode mode = SetMode::EXACT) override;
+
+private:
+  std::vector<uint8_t> data_storage;
+
+
+}; // class SimpleRegister
+
+} // end namespace llr
+
+#endif //LLR_REGISTERS_SIMPLEREGISTER_H
+
---
+++ lib/Registers/SimpleRegister.cpp
@@ -0,0 +1,26 @@
+#include "llr/Registers/SimpleRegister.h"
+#include "llr/Registers/RegisterAccessResult.h"
+
+#include <cassert>
+#include <algorithm>
+
+using namespace llvm;
+using namespace llr;
+
+using SetMode = llr::Register::SetMode;
+
+
+RegisterAccessResult SimpleRegister::get() {
+  return RegisterAccessResult(data_storage);
+}
+
+
+RegisterAccessResult SimpleRegister::set(const llvm::ArrayRef<uint8_t> data, SetMode mode) {
+  assert(data.size() == data_storage.size() && "Setting wrong length data");
+  assert(mode == SetMode::EXACT && "Only EXACT set supported");
+
+
+  std::copy(data.begin(), data.end(), data_storage.begin());
+  return RegisterAccessResult(RegisterAccessStatus::RAS_WriteOk);
+}
+
