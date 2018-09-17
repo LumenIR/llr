4c19474868923105b74f98c6c1f90a074b7c0f11
Sergey Gvozdarev <gvozdarev.sa@gmail.com>
2018-09-17 09:48:38 +0300

    Add tmp debug in LLRInterpeter

--- lib/Interpreter/LLRInterpreter.cpp
+++ lib/Interpreter/LLRInterpreter.cpp
@@ -5,8 +5,13 @@
 #include "llr/Fetcher/LLRFetcher.h"
 #include "llr/Memory/MemoryAccessResult.h"
 
+
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_os_ostream.h"
+
+
 using namespace llr;
-//using namespace llvm;
+using namespace llvm;
 
 LLRInterpreter::LLRInterpreter(LLRContext *context, LLRFetcher *f, LLRDecoder *d) :
   Ctx(context),
@@ -15,12 +20,24 @@ LLRInterpreter::LLRInterpreter(LLRContext *context, LLRFetcher *f, LLRDecoder *d
 
 }
 
+static int CNT = 0;
+
 
 void LLRInterpreter::run() {
 
 
-  while(Ctx->isRunning()) {
+  while(Ctx->isRunning() && CNT++ < 100) {
     MemoryAccessResult data = fetcher->fetchNext(*Ctx);
+    dbgs() << "Fetched 0x";
+    dbgs().write_hex(data.data[0]);
+    dbgs() << " ";
+    dbgs().write_hex(data.data[1]);
+    dbgs() << " ";
+    dbgs().write_hex(data.data[2]);
+    dbgs() << " ";
+    dbgs().write_hex(data.data[3]);
+    dbgs() << " ";
+    dbgs() << "\n";
 
     LLRInst inst = decoder->decode(data.getData(), data.getAddress());
 
