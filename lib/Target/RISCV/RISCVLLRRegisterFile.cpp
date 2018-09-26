a1ad701dca06e30a75acba71b8059f131df0a3f4
Sergey Gvozdarev <gvozdarev.sa@gmail.com>
2018-09-26 10:33:36 +0300

    [RISCV] Add x0 as ZeroRegister. And add part of CSR registers

--- lib/Target/RISCV/RISCVLLRRegisterFile.cpp
+++ lib/Target/RISCV/RISCVLLRRegisterFile.cpp
@@ -2,18 +2,60 @@
 
 #include "llr/Registers/SimpleRegister.h"
 #include "llr/Registers/ProxyRegister.h"
+#include "llr/Registers/ZeroRegister.h"
 
 using namespace llvm;
 using namespace llr;
 
 
 RISCVLLRRegisterFile::RISCVLLRRegisterFile() {
-  for (int i = 0; i < 32; ++i) {
+
+  for(unsigned CSRId : {
+                        RISCV_CSR::fflags,
+
+                        RISCV_CSR::mhartid,
+                        RISCV_CSR::mstatus,
+                        RISCV_CSR::mtvec,
+                        RISCV_CSR::satp,
+                        RISCV_CSR::pmaddr0,
+                        RISCV_CSR::pmpcfg0,
+
+                        RISCV_CSR::medeleg,
+                        RISCV_CSR::mideleg,
+                        RISCV_CSR::mie,
+                        RISCV_CSR::mepc,
+                        RISCV_CSR::mcause,
+
+                        RISCV_CSR::sstatus,
+                        RISCV_CSR::stvec,
+                        RISCV_CSR::sepc,
+                        RISCV_CSR::sscratch,
+
+                        RISCV_CSR::uepc,
+                        }) {
+
+    CSRRegisters[CSRId] = new SimpleRegister(0, 0, 4);
+  }
+
+
+
+  GPRRegisters[0] = new ZeroRegister(RISCV::X0, RISCV::GPRRegClassID, 4);
+
+  for (int i = 1; i < 32; ++i) {
     GPRRegisters[i] = new ProxyRegister(RISCV::X0 + i, RISCV::GPRRegClassID, 4, GPR_storage.data() + 4 * i);
   }
 
   PC = new SimpleRegister(0, 0, 4);
 }
+
+
+LLRRegister& RISCVLLRRegisterFile::getCSR(unsigned csrId) {
+  auto it = CSRRegisters.find(csrId);
+  assert(it != CSRRegisters.end());
+
+  return *it->second;
+}
+
 
 LLRRegister&  RISCVLLRRegisterFile::getRegisterById(unsigned regId) const {
 
--- lib/Target/RISCV/RISCVLLRRegisterFile.h
+++ lib/Target/RISCV/RISCVLLRRegisterFile.h
@@ -18,9 +18,47 @@
 
 #include <vector>
 #include <array>
+#include <unordered_map>
 
 
 namespace llr {
+
+namespace RISCV_CSR {
+  enum CSR : unsigned {
+    fflags = 0x001,
+    frm = 0x002,
+    fcsr = 0x003,
+
+    sstatus = 0x100,
+    stvec = 0x105,
+
+    uepc = 0x041,
+
+    sscratch = 0x140,
+    sepc = 0x141,
+    satp = 0x180,
+
+    mvendorid = 0xF11,
+    marchid = 0XF12,
+    mhartid = 0xF14,
+    mimpid = 0xF13,
+
+    mstatus = 0x300,
+    medeleg = 0x302,
+    mideleg = 0x303,
+    mie = 0x304,
+    mtvec = 0x305,
+
+    mepc = 0x341,
+    mcause = 0x342,
+
+    pmpcfg0 = 0x3A0,
+
+    pmaddr0 = 0x3B0,
+    pmaddr1 = 0x3B1,
+
+  };
+}
 
 class LLRRegister;
 
@@ -34,6 +72,7 @@ public:
 
   virtual LLRRegister& getPC() const override;
 
+  LLRRegister& getCSR(unsigned csrId);
 
 protected:
   LLRRegister *PC;
@@ -41,6 +80,7 @@ protected:
   std::array<uint8_t, 32*4> GPR_storage;
   std::array<LLRRegister*, 32> GPRRegisters;
 
+  std::unordered_map<unsigned, LLRRegister*> CSRRegisters;
 }; // class RISCVLLRRegisterFile
 
 } // end namespace llr
